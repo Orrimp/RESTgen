8a8595743b7d933990fd6129da15fa27c6e43b01
lsotnk <lsotnk@gmail.com>
2014-08-15 17:08:45 +0200

    Fixed Integer <-> Double

--- querylang/querylang-solr/src/main/java/de/fhws/rdsl/querylang/solr/formatters/DoubleFormatter.java
+++ querylang/querylang-solr/src/main/java/de/fhws/rdsl/querylang/solr/formatters/DoubleFormatter.java
@@ -1,20 +1,20 @@
 package de.fhws.rdsl.querylang.solr.formatters;
 
 import de.fhws.rdsl.querylang.TransformerContext;
+import de.fhws.rdsl.querylang.elements.DoubleElement;
 import de.fhws.rdsl.querylang.elements.Element;
-import de.fhws.rdsl.querylang.elements.IntegerElement;
 import de.fhws.rdsl.querylang.formatter.Formatter;
 
 public class DoubleFormatter implements Formatter {
 
     @Override
     public boolean isFormatterFor(Element element) {
-        return element instanceof IntegerElement;
+        return element instanceof DoubleElement;
     }
 
     @Override
     public String format(Element element, TransformerContext context) {
-        return ((IntegerElement) element).getValue().toString();
+        return ((DoubleElement) element).getValue().toString();
     }
 
 }
