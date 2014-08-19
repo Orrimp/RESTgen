536b16d2dc89ce6cd25866f1d2375654054c73d9
lsotnk <lsotnk@gmail.com>
2014-08-19 16:33:41 +0200

    Implemented a simple solr-parser for deleteByQuery mock method

--- riak-utils/src/main/java/de/fhws/rdsl/riak/RiakConnectionMock.java
+++ riak-utils/src/main/java/de/fhws/rdsl/riak/RiakConnectionMock.java
@@ -3,10 +3,13 @@ package de.fhws.rdsl.riak;
 import static com.google.common.collect.Maps.newHashMap;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+
+import org.json.JSONObject;
 
 import com.basho.riak.client.DefaultRiakObject;
 import com.basho.riak.client.IRiakObject;
@@ -15,6 +18,7 @@ import com.basho.riak.client.raw.FetchMeta;
 import com.basho.riak.client.raw.RiakResponse;
 import com.basho.riak.client.raw.StoreMeta;
 import com.basho.riak.client.raw.query.indexes.IndexQuery;
+import com.google.common.collect.Lists;
 
 import de.fhws.rdsl.riak.solr.RiakSolrResponse;
 
@@ -92,7 +96,44 @@ public class RiakConnectionMock implements RiakConnection {
 
     @Override
     public void deleteByQuery(String index, String query) throws IOException {
-        // TODO Auto-generated method stub
+        // parse query
+        try {
+            List<String> expressions = Arrays.asList(query.split("(and|AND)"));
+            List<String> keys = Lists.newArrayList();
+            for (String key : this.objects.keySet()) {
+                String bucket = key.split("\\.")[0];
+                if (index.equals(bucket)) {
+                    keys.add(key);
+                }
+            }
+            List<String> keysToDelete = Lists.newArrayList();
+            for (String key : keys) {
+                IRiakObject object = this.objects.get(key);
+                boolean toDelete = true;
+                for (String expression : expressions) {
+                    expression = expression.trim();
+                    String property = expression.split("\\:")[0];
+                    String value = expression.split("\\:")[1];
+                    if (value.startsWith("\"")) {
+                        value = value.substring(1, value.length() - 1);
+                    }
+                    JSONObject jsonObject = new DefaultJSONConverter().fromBytes(object.getValue(), "UTF-8");
+                    String riakValue = jsonObject.getString(property);
+                    if (!riakValue.equals(value)) {
+                        toDelete = false;
+                    }
+                }
+                if (toDelete) {
+                    keysToDelete.add(key);
+                }
+            }
+
+            for (String key : keysToDelete) {
+                this.objects.remove(key);
+            }
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
 
     }
 
